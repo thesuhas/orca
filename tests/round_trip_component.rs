30540ccf241b800e9398cc87d9353b79c91092a9
Suhas Thalanki <suhastvs@gmail.com>
2024-07-02 14:05:21 -0400

    added file writing logic

--- tests/round_trip_component.rs
+++ tests/round_trip_component.rs
@@ -1,4 +1,6 @@
 use orca::ir::Component;
+use std::io::Write; // bring trait into scope
+use std::fs::File;
 
 fn round_trip_component(testname: &str, folder: &str) {
     let filename = format!(
@@ -13,7 +15,23 @@ fn round_trip_component(testname: &str, folder: &str) {
     let result = component.encode().expect("Unable to encode");
     let out = wasmprinter::print_bytes(result).expect("couldn't translated Wasm to wat");
     let original = wasmprinter::print_bytes(buff).expect("couldn't convert original Wasm to wat");
-    assert_eq!(out, original);
+    if out != original {
+        println!("Test: {:?} failed! Writing to file to check", testname);
+
+        let mut file = match File::create(format!("{}_test.wat", testname)) {
+            Ok(file) => file,
+            Err(e) => {
+                eprintln!("Failed to create the file: {}", e);
+                return;
+            },
+        };
+
+        // Write the string to the file
+        match file.write_all(out.as_bytes()) {
+            Ok(_) => println!("Data successfully written to the file."),
+            Err(e) => eprintln!("Failed to write to the file: {}", e),
+        }
+    }
 }
 
 macro_rules! make_round_trip_tests_component {
@@ -28,16 +46,20 @@ macro_rules! make_round_trip_tests_component {
 }
 
 mod round_trip {
-    make_round_trip_tests_component!(
-        "dfinity/components",
-        data_section,
-        func,
-        func_locals,
-        table,
-        table_init,
-        exports,
-        start,
-        const_expr
-    );
+    // make_round_trip_tests_component!(
+    //     "dfinity/components",
+    //     data_section,
+    //     func,
+    //     func_locals,
+    //     table,
+    //     table_init,
+    //     exports,
+    //     start,
+    //     const_expr
+    // );
 
+    make_round_trip_tests_component!(
+        "handwritten/components",
+        add
+    );
 }
