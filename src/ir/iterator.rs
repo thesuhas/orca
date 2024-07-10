d920ec040f10d10cbebaba515d9a8b78c72fa95c
Elizabeth Gilbert <evgilber@andrew.cmu.edu>
2024-07-10 12:30:14 -0400

    fix compilation issues

--- src/ir/iterator.rs
+++ src/ir/iterator.rs
@@ -2,7 +2,6 @@ use crate::ir::component::Component;
 use crate::ir::module::Module;
 use crate::ir::types::Body;
 use wasmparser::Operator;
-use std::cell::RefCell;
 
 pub struct FuncIterator<'a> {
     curr_instr: usize,
@@ -12,13 +11,13 @@ pub struct FuncIterator<'a> {
 pub struct ModuleIterator<'a> {
     curr_func: usize,
     module: &'a Module<'a>,
-    func_iterator: RefCell<FuncIterator<'a>>,
+    func_iterator: FuncIterator<'a>,
 }
 
 pub struct ComponentIterator<'a> {
     component: &'a Component<'a>,
     curr_mod: usize,
-    mod_iterator: RefCell<ModuleIterator<'a>>,
+    mod_iterator: ModuleIterator<'a>,
 }
 
 impl<'a> FuncIterator<'a> {
@@ -29,8 +28,12 @@ impl<'a> FuncIterator<'a> {
         }
     }
 
+    pub fn has_next(&self) -> bool {
+        self.curr_instr < self.function.num_instructions
+    }
+
     pub fn next(&mut self) -> Option<&Operator> {
-        if self.curr_instr >= self.function.num_instructions {
+        if !self.has_next() {
             None
         } else {
             let instr = &self.function.instructions[self.curr_instr];
@@ -49,30 +52,37 @@ impl<'a> ModuleIterator<'a> {
         ModuleIterator {
             curr_func: 0,
             module,
-            func_iterator: RefCell::new(FuncIterator::new(&module.code_sections[0])),
+            func_iterator: FuncIterator::new(&module.code_sections[0]),
         }
+    }
+
+    pub fn has_next_function(&self) -> bool {
+        self.curr_func + 1 < self.module.num_functions
     }
 
     fn next_function(&mut self) -> bool {
         self.curr_func += 1;
         if self.curr_func < self.module.num_functions {
-            self.func_iterator.replace(FuncIterator::new(&self.module.code_sections[self.curr_func]));
+            self.func_iterator = FuncIterator::new(&self.module.code_sections[self.curr_func]);
             true
         } else {
             false
         }
     }
 
+    pub fn has_next(&self) -> bool {
+        self.func_iterator.has_next() || self.has_next_function()
+    }
+
     pub fn next(&mut self) -> Option<&Operator> {
-        match self.func_iterator.borrow_mut().next() {
-            None => {
-                if self.next_function() {
-                    self.func_iterator.borrow_mut().next()
-                } else {
-                    None
-                }
+        if self.func_iterator.has_next() {
+            self.func_iterator.next()
+        } else {
+            if self.next_function() {
+                self.func_iterator.next()
+            } else {
+                None
             }
-            Some(op) => Some(op),
         }
     }
 }
@@ -83,14 +93,14 @@ impl<'a> ComponentIterator<'a> {
         ComponentIterator {
             component: comp,
             curr_mod: 0,
-            mod_iterator: RefCell::new(ModuleIterator::new(&comp.modules[0])),
+            mod_iterator: ModuleIterator::new(&comp.modules[0]),
         }
     }
 
     fn next_module(&mut self) -> bool {
         self.curr_mod += 1;
         if self.curr_mod < self.component.num_modules {
-            self.mod_iterator.replace(ModuleIterator::new(&self.component.modules[self.curr_mod]));
+            self.mod_iterator = ModuleIterator::new(&self.component.modules[self.curr_mod]);
             true
         } else {
             false
@@ -98,15 +108,14 @@ impl<'a> ComponentIterator<'a> {
     }
 
     pub fn next(&mut self) -> Option<&Operator> {
-        match self.mod_iterator.borrow_mut().next() {
-            None => {
-                if self.next_module() {
-                    self.mod_iterator.borrow_mut().next()
-                } else {
-                    None
-                }
+        if self.mod_iterator.has_next() {
+            self.mod_iterator.next()
+        } else {
+            if self.next_module() {
+                self.mod_iterator.next()
+            } else {
+                None
             }
-            Some(op) => Some(op),
         }
     }
 }
