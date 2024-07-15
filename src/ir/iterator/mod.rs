f8c209e53a65a228565615c3b7372ecfe2ca6881
Suhas Thalanki <suhastvs@gmail.com>
2024-07-14 20:02:05 -0400

    updated module documentation

--- src/ir/iterator/mod.rs
+++ src/ir/iterator/mod.rs
@@ -1,4 +1,8 @@
-/// Iterators to traverse either a Component or a Module. Supports injection of Instructions at specific locations.
+///! Iterators to traverse either a Component or a Module. Supports injection of Instructions at specific locations.
+
+/// Iterator to traverse a Component
 pub mod component_iterator;
+/// Trait that needs to be satisfied by all iterators
 pub mod iterator_trait;
+/// Iterator to traverse a Module
 pub mod module_iterator;
--- src/ir/subiterator/mod.rs
+++ src/ir/subiterator/mod.rs
@@ -1,4 +1,8 @@
-/// Sub-iterators used by the Iterators. Keep track of current location.
+///! Sub-iterators used by the Iterators. Keep track of current location.
+
+/// SubIterator for a Component
 pub mod component_subiterator;
+/// SubIterator for a Function
 mod function_subiterator;
+/// SubIterator for a Module
 pub mod module_subiterator;
