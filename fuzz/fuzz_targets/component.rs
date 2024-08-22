42ecef43cfb657473438d23333669cfcb410e046
ahuoguo <ahuoguo@gmail.com>
2024-08-22 19:57:14 +0800

    fmt

--- fuzz/fuzz_targets/component.rs
+++ fuzz/fuzz_targets/component.rs
@@ -2,8 +2,8 @@
 #![no_main]
 
 use libfuzzer_sys::fuzz_target;
-use wasm_smith::Component;
 use orca;
+use wasm_smith::Component;
 
 fuzz_target!(|comp: Component| {
     let wasm_bytes = comp.to_bytes();
@@ -11,15 +11,17 @@ fuzz_target!(|comp: Component| {
     use std::fs::File;
     use std::io::prelude::*;
     let mut file = File::create("temp.wat").unwrap();
-    
-    let original = wasmprinter::print_bytes(wasm_bytes.clone()).expect("couldn't convert original Wasm to wat");
+
+    let original = wasmprinter::print_bytes(wasm_bytes.clone())
+        .expect("couldn't convert original Wasm to wat");
     file.write_all(&original.clone().into_bytes()).unwrap();
     // println!("original:\n {:?}", original);
 
     let mut wasm_comp = orca::Component::parse(&wasm_bytes, false).unwrap();
     let roundtrip_bytes = wasm_comp.encode();
 
-    let roundtrip = wasmprinter::print_bytes(roundtrip_bytes.clone()).expect("couldn't convert roundtrip Wasm to wat");
+    let roundtrip = wasmprinter::print_bytes(roundtrip_bytes.clone())
+        .expect("couldn't convert roundtrip Wasm to wat");
     // println!("roundtrip:\n {:?}", roundtrip);
 
     assert_eq!(original, roundtrip);
--- fuzz/fuzz_targets/module.rs
+++ fuzz/fuzz_targets/module.rs
@@ -3,8 +3,8 @@
 #![no_main]
 
 use libfuzzer_sys::fuzz_target;
-use wasm_smith::Module;
 use orca;
+use wasm_smith::Module;
 
 fuzz_target!(|module: Module| {
     let wasm_bytes = module.to_bytes();
@@ -12,15 +12,17 @@ fuzz_target!(|module: Module| {
     use std::fs::File;
     use std::io::prelude::*;
     let mut file = File::create("temp.wat").unwrap();
-    
-    let original = wasmprinter::print_bytes(wasm_bytes.clone()).expect("couldn't convert original Wasm to wat");
+
+    let original = wasmprinter::print_bytes(wasm_bytes.clone())
+        .expect("couldn't convert original Wasm to wat");
     file.write_all(&original.clone().into_bytes()).unwrap();
     // println!("original:\n {:?}", original);
 
     let mut wasm_module = orca::Module::parse(&wasm_bytes, false).unwrap();
     let roundtrip_bytes = wasm_module.encode();
 
-    let roundtrip = wasmprinter::print_bytes(roundtrip_bytes.clone()).expect("couldn't convert roundtrip Wasm to wat");
+    let roundtrip = wasmprinter::print_bytes(roundtrip_bytes.clone())
+        .expect("couldn't convert roundtrip Wasm to wat");
     // println!("roundtrip:\n {:?}", roundtrip);
 
     assert_eq!(original, roundtrip);
