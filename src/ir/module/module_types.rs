79e3915297c4a0221c48d6365ddc4d7c6a6ed11c
Suhas Thalanki <suhastvs@gmail.com>
2024-10-31 15:19:48 -0400

    added gc instructions for injection

--- src/ir/module/module_types.rs
+++ src/ir/module/module_types.rs
@@ -4,7 +4,7 @@ use crate::ir::id::TypeID;
 use crate::DataType;
 use std::collections::HashMap;
 use std::hash::Hash;
-use wasmparser::{PackedIndex, StorageType};
+use wasmparser::{PackedIndex, StorageType, UnpackedIndex};
 
 // Orca's representation of function types, shortened from [Walrus' Representation].
 //
@@ -98,5 +98,96 @@ impl ModuleTypes {
     /// Get type from index of the type section
     pub fn get(&self, index: TypeID) -> Option<&Types> {
         self.types.get(*index as usize)
+    }
+}
+
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+pub enum HeapType {
+    Abstract { shared: bool, ty: AbstractHeapType },
+    // TODO: See to replace UnpackedIndex with `orca` specific implementation
+    Concrete(UnpackedIndex),
+}
+
+impl From<wasmparser::HeapType> for HeapType {
+    fn from(value: wasmparser::HeapType) -> Self {
+        match value {
+            wasmparser::HeapType::Abstract { shared, ty } => HeapType::Abstract {
+                shared,
+                ty: AbstractHeapType::from(ty),
+            },
+            wasmparser::HeapType::Concrete(idx) => HeapType::Concrete(idx),
+        }
+    }
+}
+
+impl From<HeapType> for wasmparser::HeapType {
+    fn from(value: HeapType) -> Self {
+        match value {
+            HeapType::Abstract { shared, ty } => wasmparser::HeapType::Abstract {
+                shared,
+                ty: wasmparser::AbstractHeapType::from(ty),
+            },
+            HeapType::Concrete(idx) => wasmparser::HeapType::Concrete(idx),
+        }
+    }
+}
+
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+pub enum AbstractHeapType {
+    Func,
+    Extern,
+    Any,
+    None,
+    NoExtern,
+    NoFunc,
+    Eq,
+    Struct,
+    Array,
+    I31,
+    Exn,
+    NoExn,
+    Cont,
+    NoCont,
+}
+
+impl From<wasmparser::AbstractHeapType> for AbstractHeapType {
+    fn from(value: wasmparser::AbstractHeapType) -> Self {
+        match value {
+            wasmparser::AbstractHeapType::Func => AbstractHeapType::Func,
+            wasmparser::AbstractHeapType::Extern => AbstractHeapType::Extern,
+            wasmparser::AbstractHeapType::Any => AbstractHeapType::Any,
+            wasmparser::AbstractHeapType::None => AbstractHeapType::None,
+            wasmparser::AbstractHeapType::NoExtern => AbstractHeapType::NoExtern,
+            wasmparser::AbstractHeapType::NoFunc => AbstractHeapType::NoFunc,
+            wasmparser::AbstractHeapType::Eq => AbstractHeapType::Eq,
+            wasmparser::AbstractHeapType::Struct => AbstractHeapType::Struct,
+            wasmparser::AbstractHeapType::Array => AbstractHeapType::Array,
+            wasmparser::AbstractHeapType::I31 => AbstractHeapType::I31,
+            wasmparser::AbstractHeapType::Exn => AbstractHeapType::Exn,
+            wasmparser::AbstractHeapType::NoExn => AbstractHeapType::NoExn,
+            wasmparser::AbstractHeapType::Cont => AbstractHeapType::Cont,
+            wasmparser::AbstractHeapType::NoCont => AbstractHeapType::NoCont,
+        }
+    }
+}
+
+impl From<AbstractHeapType> for wasmparser::AbstractHeapType {
+    fn from(value: AbstractHeapType) -> Self {
+        match value {
+            AbstractHeapType::Func => wasmparser::AbstractHeapType::Func,
+            AbstractHeapType::Extern => wasmparser::AbstractHeapType::Extern,
+            AbstractHeapType::Any => wasmparser::AbstractHeapType::Any,
+            AbstractHeapType::None => wasmparser::AbstractHeapType::None,
+            AbstractHeapType::NoExtern => wasmparser::AbstractHeapType::NoExtern,
+            AbstractHeapType::NoFunc => wasmparser::AbstractHeapType::NoFunc,
+            AbstractHeapType::Eq => wasmparser::AbstractHeapType::Eq,
+            AbstractHeapType::Struct => wasmparser::AbstractHeapType::Struct,
+            AbstractHeapType::Array => wasmparser::AbstractHeapType::Array,
+            AbstractHeapType::I31 => wasmparser::AbstractHeapType::I31,
+            AbstractHeapType::Exn => wasmparser::AbstractHeapType::Exn,
+            AbstractHeapType::NoExn => wasmparser::AbstractHeapType::NoExn,
+            AbstractHeapType::Cont => wasmparser::AbstractHeapType::Cont,
+            AbstractHeapType::NoCont => wasmparser::AbstractHeapType::NoCont,
+        }
     }
 }
